ed5e2a778598578136f598163cbcd1fc3ad7a339
John A Maloney <john.a.maloney@gmail.com>
2018-02-14 21:03:22 -0600

    bootstrap 3.3.7 explicit dependency

---
+++ src/ApproveDeny.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+class ApproveDeny extends React.Component {
+  render () {
+    return (
+      <label className="switch">
+        <input type="checkbox">
+        <span className="slider round"></span>
+      </label>
+    )
+  }
+}
