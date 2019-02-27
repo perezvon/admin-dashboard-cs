97f7e4bfa15139eac755a1cc3e364f16ecde5461
John A Maloney <john.a.maloney@gmail.com>
2019-02-26 22:36:54 -0600

    Increased request size limit

--- server.js
+++ server.js
@@ -13,10 +13,10 @@ const app = express();
 const port = process.env.PORT || 3001;
 
 //support parsing of application/json type post data
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: '5mb'}));
 
 //support parsing of application/x-www-form-urlencoded post data
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true, limit: '5mb' }));
 
 app.use(express.static(path.resolve(__dirname, 'build')));
 
